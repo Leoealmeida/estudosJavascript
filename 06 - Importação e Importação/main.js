d6890bf579d84de862cd55cfaca1a110b6fb484b
Leoealmeida <leo.ealmeida@gmail.com>
2023-09-29 15:32:22 -0300

    Inclusão de novos arquivos de estudo

---
+++ 06 - Importação e Importação/main.js
@@ -0,0 +1,14 @@
+
+const { gets, print } = require ('./funcoes-auxiliares');
+
+
+let maiorValorEncontrado = 0;
+
+for (let i = 0; i < 5; i++) {
+    const numeroSorteado = gets();
+    if (numeroSorteado > maiorValorEncontrado){
+        maiorValorEncontrado = numeroSorteado;
+    }
+}
+
+print(maiorValorEncontrado);
